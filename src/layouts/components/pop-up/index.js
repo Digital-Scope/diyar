90e62942a7fc04cd45db696fe483bb9f3370c744
Abed Fleyfel <abed.fleyfel@hicmedia.com>
2021-08-12 11:07:12 +0300

    update for popup

--- src/layouts/components/pop-up/index.js
+++ src/layouts/components/pop-up/index.js
@@ -23,8 +23,6 @@ export default class PopUp extends Component {
   }
 
   isValidDate() {
-    console.log('abed');
-    console.log(this.props);
     const currendDate = Date.now();
     if (currendDate >= this.StartDate && currendDate <= this.EndDate) return true;
     return false;
